4792e3700245b0d3806ab75a1f188c50da3a72c9
ibrahemesam <iimosa7777@gmail.com>
2024-05-24 01:41:30 +0300

    set current city  info on home_page && fetch its forecast ondemand

--- api/v1/views/main.py
+++ api/v1/views/main.py
@@ -7,5 +7,40 @@ from fastapi.templating import Jinja2Templates
 templates = Jinja2Templates(directory="../../static")
 @main_router.get("/")
 async def main(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    """Main page"""
+    ip = request.headers.get('X-Forwarded-For')
+    try:
+        responser = get(f'https://freegeoip.app/json/{ip}').json()
+    except Exception:
+        try:
+            responser = get(f'http://ip-api.com/json/{ip}').json()
+        except Exception:
+            responser = {'city': None, 'country_code': None}
+    longi = responser.get('longitude')
+    lati = responser.get('latitude')
+    params = {'lat': lati, 'lon': longi,
+              'appid': 'bacd9cc5a7f5a2ac5b557498678ed9d0',
+                'units': 'metric'}
+    try:
+        responser_w = get('https://api.openweathermap.org/data/2.5/weather', params=params).json()
+    except Exception:
+        responser_w = {'main': {'temp': None}}
+    if __debug__:
+        if ip is None:
+            responser_w = {'main': {'temp': 38, 'humidity': 25}, 'name': 'Giza', 'weather': [{'description': 'Hell', 'icon': '01d'}], 'wind': {'speed': 25, 'deg': 255}, 'timezone': 10800}
+            responser = {'country_name': 'Egypt'}
+    return templates.TemplateResponse("index.html", {
+        "request": request,
+        "city": responser_w.get('name'),
+        "country": responser.get('country_name'),
+        "temp": responser_w.get('main').get('temp'),
+        "description": responser_w.get('weather')[0].get('description'),
+        'icon': responser_w['weather'][0]['icon'],
+        'humidity': responser_w['main']['humidity'],
+        'wind': {
+            'speed': responser_w['wind']['speed'],
+            'deg': responser_w['wind']['deg']
+        },
+        'timezone': responser_w['timezone']
+    })
 # --------------- #
--- static/js/main.js
+++ static/js/main.js
@@ -10,29 +10,38 @@ const elInputCity = document.getElementById("inp-city");
 const app = Vue.createApp({
   data() {
     // fetch("https://example.com?");
-
+    /* 
+    window.initialInfo = {
+        city: "{{city}}",
+        country: "{{country}}",
+        temp: {{temp}},
+        description: "{{description}}"
+      };
+    */
+    window.initialInfo.time = moment();
     return {
       // inputCity: (await fetch("http://for-us.tech")).json().city,
       msg: "Please select a City",
       inputCity: "",
       selectedWeatherType: "weather",
       selectedUnit: "C",
-      infoReady: false,
+      infoReady: true,
       // dailyForecast: [],
       // weeklyForecast: [],
       current: {
-        time: "6:00 PM",
-        temp: 30,
-        condition: "Clear sky",
-        city: "Giza",
-        country: "EG",
-        date: "Mon 15 Oct 2024",
-        humidity: 25,
+        timezone: initialInfo.timezone,
+        time: initialInfo.time.format("h:mm A"),
+        temp: initialInfo.temp,
+        condition: initialInfo.description,
+        city: initialInfo.city,
+        country: initialInfo.country,
+        date: initialInfo.time.format("ddd D MMM YYYY"),
+        humidity: initialInfo.humidity,
         wind: {
-          speed: 26,
-          deg: 255,
+          speed: initialInfo.wind.speed,
+          deg: initialInfo.wind.deg,
         },
-        icon: "01d",
+        icon: initialInfo.icon,
       },
       weeklyForecast: [],
       dailyForecast: [],
@@ -65,6 +74,7 @@ const app = Vue.createApp({
       var timezone = res.timezone;
       var time = moment(res.dt * 1000 + timezone);
       this.current = {
+        timezone: timezone,
         time: time.format("h:mm A"),
         temp: res.main.temp,
         condition: res.weather[0].description,
@@ -78,59 +88,69 @@ const app = Vue.createApp({
         },
         icon: res.weather[0].icon,
       };
-      /* fetch forecast */
-      // daily
-      res = await fetch(
-        `${API_URL}/api/v1/forecast?` +
-          new URLSearchParams({
-            city: this.inputCity,
-            time_type: "hourly",
-          })
-      );
-      res = await res.json();
-      this.dailyForecast.length = 0;
-      for (let i of res.list) {
-        this.dailyForecast.push({
-          time: moment(i.dt * 1000 + timezone).format("h:mm A"),
-          temp: i.main.temp,
-          condition: i.weather[0].description,
-          humidity: i.main.humidity,
-          wind: {
-            speed: i.wind.speed,
-            deg: i.wind.deg,
-          },
-          icon: i.weather[0].icon,
-        });
-      }
-      // weekly
-      res = await fetch(
-        `${API_URL}/api/v1/forecast?` +
-          new URLSearchParams({
-            city: this.inputCity,
-            time_type: "weekly",
-          })
-      );
-      res = await res.json();
-      this.weeklyForecast.length = 0;
-      var last_day = undefined,
-        day;
-      for (let i of res.list) {
-        day = moment(i.dt * 1000 + timezone).format("ddd");
-        if (day === last_day) continue;
-        last_day = day;
-        this.weeklyForecast.push({
-          time: day,
-          temp: i.main.temp,
-          condition: i.weather[0].description,
-          humidity: i.main.humidity,
-          wind: {
-            speed: i.wind.speed,
-            deg: i.wind.deg,
-          },
-          icon: i.weather[0].icon,
-        });
-      }
+      this.fetchForecast();
       this.infoReady = true;
+    },
+    async fetchForecast() {
+      if (
+        this.current.city &&
+        !this.dailyForecast.length &&
+        !this.weeklyForecast.length
+      ) {
+        /* fetch forecast */
+        var timezone = this.current.timezone;
+        // daily
+        res = await fetch(
+          `${API_URL}/api/v1/forecast?` +
+            new URLSearchParams({
+              city: this.current.city,
+              time_type: "hourly",
+            })
+        );
+        res = await res.json();
+        this.dailyForecast.length = 0;
+        for (let i of res.list) {
+          this.dailyForecast.push({
+            time: moment(i.dt * 1000 + timezone).format("h:mm A"),
+            temp: i.main.temp,
+            condition: i.weather[0].description,
+            humidity: i.main.humidity,
+            wind: {
+              speed: i.wind.speed,
+              deg: i.wind.deg,
+            },
+            icon: i.weather[0].icon,
+          });
+        }
+        // weekly
+        res = await fetch(
+          `${API_URL}/api/v1/forecast?` +
+            new URLSearchParams({
+              city: this.current.city,
+              time_type: "weekly",
+            })
+        );
+        res = await res.json();
+        this.weeklyForecast.length = 0;
+        var last_day = undefined,
+          day;
+        for (let i of res.list) {
+          day = moment(i.dt * 1000 + timezone).format("ddd");
+          if (day === last_day) continue;
+          last_day = day;
+          this.weeklyForecast.push({
+            time: day,
+            temp: i.main.temp,
+            condition: i.weather[0].description,
+            humidity: i.main.humidity,
+            wind: {
+              speed: i.wind.speed,
+              deg: i.wind.deg,
+            },
+            icon: i.weather[0].icon,
+          });
+        }
+      }
     },
     convertUnit(temp) {
       if (this.selectedUnit === "F") return ((temp * 9) / 5 + 32).toFixed(2);
